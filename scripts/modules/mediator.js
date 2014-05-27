ca79107f8ab0aa03e8b33f5ab050fd612caf5367
Isaac Rankin <isaac.webfix@gmail.com>
2014-05-27 10:51:43 +1000

    Converted indents to tabs

--- scripts/modules/mediator.js
+++ scripts/modules/mediator.js
@@ -1,42 +1,40 @@
 var Mediator = function(){
 
-    'use strict';
+	'use strict';
 
-    var subscribe = function(channel, fn){
-        if (!this.channels[channel]) {
-            this.channels[channel] = [];
-        }
-        this.channels[channel].push({
-            context: this,
-            callback: fn
-        });
-        return this;
-    },
+	var subscribe = function(channel, fn){
+		if (!this.channels[channel]) {
+			this.channels[channel] = [];
+		}
+		this.channels[channel].push({
+			context: this,
+			callback: fn
+		});
+		return this;
+	},
 
-    publish = function(channel){
-        if (!this.channels[channel]) {
-            return false;
-        }
+	publish = function(channel){
+		if (!this.channels[channel]) {
+			return false;
+		}
 
-        var args = Array.prototype.slice.call(arguments, 1);
+		var args = Array.prototype.slice.call(arguments, 1);
 
-        for (var i = 0, l = this.channels[channel].length; i < l; i++) {
-            var subscription = this.channels[channel][i];
-            subscription.callback.apply(subscription.context, args);
-        }
+		for (var i = 0, l = this.channels[channel].length; i < l; i++) {
+			var subscription = this.channels[channel][i];
+			subscription.callback.apply(subscription.context, args);
+		}
 
-        console.info('publish: '+ channel);
+		return this;
+	};
 
-        return this;
-    };
-
-    return {
-        channels: {},
-        publish: publish,
-        subscribe: subscribe,
-        installTo: function(obj){
-            obj.subscribe = subscribe;
-            obj.publish = publish;
-        }
-    };
+	return {
+		channels: {},
+		publish: publish,
+		subscribe: subscribe,
+		installTo: function(obj){
+			obj.subscribe = subscribe;
+			obj.publish = publish;
+		}
+	};
 };
